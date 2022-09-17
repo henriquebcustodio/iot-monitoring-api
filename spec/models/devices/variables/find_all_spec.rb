628d71a4d20dc7fc6825afe6f64fd98ce6d4d61b
Henrique Bonfá Custódio <henriquebcustodio@outlook.com>
2022-09-17 18:35:27 -0300

    Add missing test to Devices::Variables::FindAll

--- spec/models/devices/variables/find_all_spec.rb
+++ spec/models/devices/variables/find_all_spec.rb
@@ -2,37 +2,65 @@ require 'rails_helper'
 
 RSpec.describe Devices::Variables::FindAll do
   describe '.call' do
-    describe 'success' do
-      let(:device) { create(:device) }
+    describe 'failures' do
+      context 'with an invalid device' do
+        it 'returns a failure' do
+          # given
+          device = ['', [], {}].sample
 
-      it 'returns a success' do
-        # given
-        create(:variable, label: 'variable1', device:)
-        create(:variable, label: 'variable2', device:)
+          # when
+          result = described_class.call(device:)
 
-        # when
-        result = described_class.call(device:)
+          # then
+          expect(result).to be_a_failure
+        end
 
-        # then
-        expect(result).to be_a_success
+        it 'exposes the error' do
+          # given
+          device = ['', [], {}].sample
+
+          # when
+          result = described_class.call(device:)
+
+          # then
+          expect(result.type).to be(:invalid_attributes)
+        end
       end
+    end
 
-      it 'exposes the devices' do
-        # given
-        variable1 = create(:variable, label: 'variable1', device:)
-        variable2 = create(:variable, label: 'variable2', device:)
+    describe 'success' do
+      context 'with a valid device' do
+        let(:device) { create(:device) }
 
-        # when
-        result = described_class.call(device:)
+        it 'returns a success' do
+          # given
+          create(:variable, label: 'variable1', device:)
+          create(:variable, label: 'variable2', device:)
 
-        # then
-        expect(result[:variables])
-          .to match(
-            [
-              variable1,
-              variable2
-            ]
-          )
+          # when
+          result = described_class.call(device:)
+
+          # then
+          expect(result).to be_a_success
+        end
+
+        it 'exposes the devices' do
+          # given
+          variable1 = create(:variable, label: 'variable1', device:)
+          variable2 = create(:variable, label: 'variable2', device:)
+
+          # when
+          result = described_class.call(device:)
+
+          # then
+          expect(result[:variables])
+            .to match(
+              [
+                variable1,
+                variable2
+              ]
+            )
+        end
       end
     end
   end
